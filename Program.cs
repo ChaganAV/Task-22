c63e30cb51659bdbc942edba1a11e0f189c9fda2
Alexandr <jwchaganav@gmail.com>
2022-10-20 22:47:54 +0400

    end

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите количество раз: ");
+int count = 1;
+
+while(count <= num)
+{
+    if (count == num)
+        Console.Write($"{count*count}");
+    else
+        Console.Write($"{count*count}, ");
+    count++;
+}
